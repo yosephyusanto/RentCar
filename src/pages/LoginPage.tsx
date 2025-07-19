fb9a4705b04a419561e31b8a1013c828a9c75aed
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-07-19 22:35:20 +0700

    create login and register page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,31 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import type {
+  AssignRoleRequest,
+  LoginRequest,
+  LoginResponse,
+} from "../interfaces/auth";
+import { assignRole, login } from "../services/auth";
+import { toast } from "sonner";
 
 const LoginPage = () => {
+  const [data, setData] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors: { email: string; password: string } = {
+      email: "",
+      password: "",
+    };
+
+    if (!data.email) {
+      newErrors.email = "Email is required";
+      isValid = false;
+    }
+
+    if (!data.password) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!validateForm()) return;
+
+    try {
+      const response: LoginResponse = await login(data);
+      localStorage.setItem("token", response.token);
+      const temp: AssignRoleRequest = { email: data.email, role: "customer" };
+      const message: string = await assignRole(temp);
+      console.log(message);
+    } catch (e: any) {
+      toast.error(e.message || "Something went wrong when sending the data");
+    }
+  };
+
   return (
     <div className="relative min-h-screen flex justify-center items-center overflow-hidden">
       {/* white card */}
       <div className="bg-white shadow-2xl p-8 rounded-md w-full sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
         <h3 className="text-center text-2xl font-bold mb-4">Login</h3>
-        <form action="" className="space-y-4 lg:space-y-8">
+        <form onSubmit={handleLogin} className="space-y-4 lg:space-y-8">
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
             <input
               type="text"
               name="email"
+              onChange={handleChange}
               className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.email && (
+              <span className="text-red-500 text-sm">{errors.email}</span>
+            )}
           </div>
           <div className="flex flex-col">
             <label htmlFor="password">Password</label>
             <input
               type="text"
               name="password"
+              onChange={handleChange}
               className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.password && (
+              <span className="text-red-500 text-sm">{errors.password}</span>
+            )}
           </div>
           <div>
-            <button className="w-full bg-blue-600 px-4 py-3 rounded-md font-semibold text-white hover:bg-blue-800 transition-colors duration-200 cursor-pointer">
+            <button
+              type="submit"
+              className="w-full bg-blue-600 px-4 py-3 rounded-md font-semibold text-white hover:bg-blue-800 transition-colors duration-200 cursor-pointer"
+            >
               Login
             </button>
           </div>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,69 +1,212 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import type { RegisterRequest } from "../interfaces/auth";
+import { register } from "../services/auth";
+import { toast } from "sonner";
 
 const RegisterPage = () => {
+  const [data, setData] = useState<RegisterRequest>({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    fullName: "",
+    phoneNumber: "",
+    address: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    fullName: "",
+    phoneNumber: "",
+    address: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      fullName: "",
+      phoneNumber: "",
+      address: "",
+    };
+
+    // Validate Full Name
+    if (!data.fullName) {
+      newErrors.fullName = "Full Name is required";
+      isValid = false;
+    }
+
+    // Validate Email
+    if (!data.email) {
+      newErrors.email = "Email is required";
+      isValid = false;
+    }
+
+    // Validate Password
+    if (!data.password) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
+
+    // Validate Confirm Password
+    if (!data.confirmPassword) {
+      newErrors.confirmPassword = "Confirm Password is required";
+      isValid = false;
+    } else if (data.password !== data.confirmPassword) {
+      newErrors.confirmPassword = "Passwords do not match";
+      isValid = false;
+    }
+
+    // Validate Phone Number
+    if (!data.phoneNumber) {
+      newErrors.phoneNumber = "Phone Number is required";
+      isValid = false;
+    }
+
+    // Validate Address
+    if (!data.address) {
+      newErrors.address = "Address is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validate before proceeding
+    if (!validateForm()) return;
+
+    try {
+      const response: string = await register(data);
+      toast.success(response); // Assuming register() returns a success message
+    } catch (e: any) {
+      toast.error(e.message || "Something went wrong when sending the data");
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen w-screen ">
+    <div className="flex justify-center items-center h-screen w-screen">
       <div className="bg-white p-2 rounded-md shadow-2xl md:w-xl md:p-8">
         <h3 className="text-center text-2xl font-bold mb-4">Register</h3>
         <form
-          action=""
+          onSubmit={handleRegister}
           className="space-y-2 md:space-y-4 lg:space-y-6 xl:space-y-8"
         >
+          {/* Fullname Input */}
           <div className="flex flex-col">
-            <label htmlFor="fullname">Fullname</label>
+            <label htmlFor="fullName">Fullname</label>
             <input
               type="text"
-              name="fullname"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+              name="fullName"
+              onChange={handleChange}
+              value={data.fullName}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.fullName && (
+              <span className="text-red-500 text-sm">{errors.fullName}</span>
+            )}
           </div>
+
+          {/* Email Input */}
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
             <input
               type="text"
               name="email"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+              onChange={handleChange}
+              value={data.email}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.email && (
+              <span className="text-red-500 text-sm">{errors.email}</span>
+            )}
           </div>
+
+          {/* Password Input */}
           <div className="flex flex-col">
             <label htmlFor="password">Password</label>
             <input
-              type="text"
+              type="password"
               name="password"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+              onChange={handleChange}
+              value={data.password}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.password && (
+              <span className="text-red-500 text-sm">{errors.password}</span>
+            )}
           </div>
+
+          {/* Confirm Password Input */}
           <div className="flex flex-col">
-            <label htmlFor="confirm_password">Confirm Password</label>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              onChange={handleChange}
+              value={data.confirmPassword}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+            />
+            {errors.confirmPassword && (
+              <span className="text-red-500 text-sm">
+                {errors.confirmPassword}
+              </span>
+            )}
+          </div>
+
+          {/* Phone Number Input */}
+          <div className="flex flex-col">
+            <label htmlFor="phoneNumber">Phone Number</label>
             <input
               type="text"
-              name="confirm_password"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="phonenumber">Phone number</label>
-            <input
-              type="text"
-              name="phonenumber"
+              name="phoneNumber"
               placeholder="+62 XXX-XXXX-XXXX"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+              onChange={handleChange}
+              value={data.phoneNumber}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.phoneNumber && (
+              <span className="text-red-500 text-sm">{errors.phoneNumber}</span>
+            )}
           </div>
+
+          {/* Address Input */}
           <div className="flex flex-col">
-            <label htmlFor="alamat">Alamat</label>
+            <label htmlFor="address">Address</label>
             <input
               type="text"
-              name="alamat"
-              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3  focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
+              name="address"
+              onChange={handleChange}
+              value={data.address}
+              className="border border-gray-300 focus:outline-none focus:ring-0 rounded-lg px-4 py-3 focus:border-blue-500 focus:border-2 transition-all duration-200 placeholder-gray-400"
             />
+            {errors.address && (
+              <span className="text-red-500 text-sm">{errors.address}</span>
+            )}
           </div>
+
+          {/* Register Button */}
           <div>
-            <button className="w-full text-white font-semibold bg-blue-600 px-4 py-3 rounded-md hover:bg-blue-800 transition-colors duration-200 cursor-pointer">
+            <button
+              type="submit"
+              className="w-full text-white font-semibold bg-blue-600 px-4 py-3 rounded-md hover:bg-blue-800 transition-colors duration-200 cursor-pointer"
+            >
               Register
             </button>
           </div>
+
+          {/* Login Link */}
           <div className="text-center">
             Already have an account?{" "}
             <span className="text-blue-400 cursor-pointer hover:underline">
