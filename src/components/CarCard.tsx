a6d4546ae3a15254362ddf1715af579cdbae5f00
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-07-19 22:36:00 +0700

    fixing styling for filtering section at homepage

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -11,23 +11,23 @@ type CarCardProps = {
 const CarCard = ({ data }: CarCardProps) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const imageUrl = data.imageLink ? baseImageApi + data.imageLink : null;
-  const formatPrice = (price : number) => {
-    return price.toLocaleString('id-ID');
-  }
+  const formatPrice = (price: number) => {
+    return price.toLocaleString("id-ID");
+  };
 
   const handleModal = () => {
     setShowModal(!showModal);
-  }
+  };
 
   return (
     // max-w-xs
-    <div className=" w-full bg-white rounded-2xl shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-xl">
+    <div className=" w-full bg-white rounded-2xl  shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-xl">
       {/* Image Section */}
       <div className="relative w-full h-48">
         <img
           className="w-full h-full object-cover rounded-t-2xl"
           src={imageUrl || NoImage}
-          alt={data.name} 
+          alt={data.name}
         />
         <div className="absolute top-0 left-0 bg-gradient-to-t from-black opacity-40 w-full h-full rounded-t-2xl"></div>
       </div>
@@ -38,13 +38,19 @@ const CarCard = ({ data }: CarCardProps) => {
           {data.name}
         </h3>
         <p className="text-lg font-medium text-blue-600 mt-2">
-           <span className="font-semibold">Rp. {formatPrice(data.price_per_day)}</span> <span className="text-gray-500 text-sm">/hari</span>
+          <span className="font-semibold">
+            Rp. {formatPrice(data.price_per_day)}
+          </span>{" "}
+          <span className="text-gray-500 text-sm">/hari</span>
         </p>
       </div>
 
       {/* Button Section */}
       <div className="p-4 pt-0">
-        <button onClick={handleModal} className="w-full py-2 text-white font-semibold bg-blue-600 rounded-xl transition-all duration-300 hover:bg-blue-700 transform hover:scale-105">
+        <button
+          onClick={handleModal}
+          className="w-full py-2 text-white font-semibold bg-blue-600 rounded-xl transition-all duration-300 hover:bg-blue-700 transform hover:scale-105"
+        >
           <span className="flex justify-center items-center">
             {/* <span className="mr-2">🚗</span> */}
             Detail
@@ -53,7 +59,13 @@ const CarCard = ({ data }: CarCardProps) => {
       </div>
 
       {/* Card Detail Modal */}
-      {showModal && <CarDetail formatPrice={formatPrice} onClose={handleModal} carId={data.car_id}/>}
+      {showModal && (
+        <CarDetail
+          formatPrice={formatPrice}
+          onClose={handleModal}
+          carId={data.car_id}
+        />
+      )}
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -27,8 +27,94 @@ const HomePage = () => {
 
   return (
     <>
-    {/* <div className="fixed bg-black opacity-50 top-0 left-0 right-0 bottom-0 z-50"></div> */}
       <div className="md:max-w-7xl lg:max-w-screen-2xl mx-auto px-10 sm:px-8 md:px-6 lg:px4 xl:px-0">
+        {/* Filtering */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  mb-16">
+          {/* Pickup Date */}
+          <div className="space-y-2">
+            <label
+              htmlFor="pickupDate"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Pickup Date
+            </label>
+            <div className="relative">
+              <input
+                type="date"
+                id="pickupDate"
+                name="pickupDate"
+                // value={filters.pickupDate}
+                // onChange={handleFilterChange}
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
+              />
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                {/* icon */}
+              </div>
+            </div>
+          </div>
+          {/* Return Date */}
+          <div className="space-y-2">
+            <label
+              htmlFor="returnDate"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Return Date
+            </label>
+            <div className="relative">
+              <input
+                type="date"
+                id="returnDate"
+                name="returnDate"
+                // value={filters.returnDate}
+                // onChange={handleFilterChange}
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
+              />
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                {/* icon */}
+              </div>
+            </div>
+          </div>
+          {/* Production Year */}
+          <div className="space-y-2 sm:col-span-2 md:col-span-1">
+            <label
+              htmlFor="productionYear"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              Production Year
+            </label>
+            <div className="relative">
+              <select
+                id="productionYear"
+                name="productionYear"
+                // value={filters.productionYear}
+                // onChange={handleFilterChange}
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white appearance-none"
+              >
+                <option value="">Any Year</option>
+                <option value="2024">2024</option>
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+              </select>
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                {/* icon */}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row sm:justify-end  sm:col-span-2  md:col-span-3 md:justify-end gap-3  lg:col-span-1 lg:justify-start lg:items-end">
+            <button
+              // onClick={handleSearch}
+              className="flex-1 sm:flex-none lg:h-[49.33px] bg-gradient-to-r from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800 text-white font-semibold px-8 py-3 rounded-lg transition-all duration-200 transform hover:scale-105 hover:shadow-lg flex items-center justify-center gap-2 cursor-pointer"
+            >
+              Search Cars
+            </button>
+          </div>
+        </div>
+
+        {/* Cars List */}
         <div className="min-h-screen flex flex-col">
           {error ? (
             <div className="flex flex-1 justify-center items-center">
