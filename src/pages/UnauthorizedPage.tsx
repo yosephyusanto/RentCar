8864833f5d91926087fe0d6ea2a3256695e16e2e
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-08-08 21:12:26 +0700

    unathourized page

--- src/pages/UnauthorizedPage.tsx
+++ src/pages/UnauthorizedPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const UnauthorizedPage = () => {
-  return <div>UnauthorizedPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen flex flex-row justify-center items-center">
+      <div className="flex flex-col items-center">
+        <p className="text-gray-500 text-9xl opacity-70">401</p>
+        <p className="text-xl">Unauthorized Page</p>
+        <button onClick={() => navigate('/')} className="mt-4 bg-blue-600 text-white py-2 px-4 rounded-md cursor-pointer">Return Home</button>
+      </div>
+    </div>
+  );
 };
 
 export default UnauthorizedPage;
