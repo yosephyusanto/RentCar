2e7aada1a7f8675727a798e36e9a264ae86efea8
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-08-14 18:27:22 +0700

    change baseapi to deployed rent car rest api

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,3 @@
-export const baseApi = "https://localhost:7116/api";
+// export const baseApi = "https://localhost:7116/api";
+export const baseApi = "https://rentcarrestapi-production.up.railway.app/api";
 // export const baseImageApi = "https://localhost:7116";
