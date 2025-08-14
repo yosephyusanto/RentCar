2b66c70b2a03034c4787a851e479371151d0dc9c
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-08-14 18:39:52 +0700

    add configuration to deploy vite to vercell

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  base: process.env.VITE_BASE_PATH || "/RentCar",
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
